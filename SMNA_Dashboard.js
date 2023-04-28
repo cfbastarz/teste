3b63c9028e199bbdd3a7491aa03f769c5159da9b
Carlos Frederico Bastarz <cfbastarz@gmail.com>
2023-04-28 16:19:22 -0300

    Teste

--- SMNA_Dashboard.js
+++ SMNA_Dashboard.js
@@ -63,9 +63,10 @@ from matplotlib import pyplot as plt
 
 pn.extension(sizing_mode="stretch_width")
 
-path = './pkl'
+#path = './pkl'
 
-dfs = pk.load(open(os.path.join(path,'jo_table_series.pkl'), 'rb'))
+#dfs = pk.load(open(os.path.join(path,'jo_table_series.pkl'), 'rb'))
+dfs = pk.load(open('jo_table_series.pkl', 'rb'))
 
 df_dtc = dfs.df_dtc
 df_bamh_T0 = dfs.df_bamh_T0
